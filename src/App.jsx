2b14f52032b8b3c8143621805a573da22bea2658
AnotherRizz <risqijapana55368@gmail.com>
2025-05-15 11:16:05 +0700

    create a project and install tools

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import {House} from 'lucide-react';
+
+function App() {
+
+  return (
+    <>
+       <h1 class="text-3xl font-bold underline font-poppins">
+    Hello world!
+  </h1>
+  
+    </>
+  )
+}
+
+export default App
